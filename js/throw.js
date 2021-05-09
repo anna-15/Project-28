7104b3a124ee6c0c364f7cd2323c2a2b50ffc84c
anna-15 <79515389+anna-15@users.noreply.github.com>
2021-05-09 12:15:21 +0530

    Add files via upload

---
+++ js/throw.js
@@ -0,0 +1,30 @@
+class Throw{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.01,
+            length: 10
+        }
+        this.pointB=pointB;
+        this.throw = Constraint.create(options);
+        World.add(world, this.throw);
+    }
+
+    fly(){
+        this.throw.bodyA=null;
+    }
+
+    Launch(bodyA){
+        this.throw.bodyA=bodyA;
+    }
+
+    display(){
+        var pointA = this.throw.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+      //  line(pointA.x, pointA.y, pointB.x, pointB.y);
+       
+    }
+    
+}
